056ecdd863e8ab40c5ab4b5bd7258a1124cffd90
amardeep <amardeepeng99@gmail.com>
2026-02-07 13:04:38 +0530

    Add Allure reporting and fix TestNG execution

--- src/main/java/com/retail/ui/automation/base/BaseTest.java
+++ src/main/java/com/retail/ui/automation/base/BaseTest.java
@@ -1,16 +1,21 @@
 package com.retail.ui.automation.base;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
 
 public class BaseTest {
 
@@ -47,10 +52,19 @@ public class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
         System.out.println(">>> TEARDOWN CALLED <<<");
+        if (ITestResult.FAILURE == result.getStatus()) {
+            Allure.addAttachment(
+                "Failure Screenshot",
+                new ByteArrayInputStream(
+                    ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)
+                )
+            );
+        }
+
         if (driver != null) {
             driver.quit();
         }
-    }
+   }
 }
